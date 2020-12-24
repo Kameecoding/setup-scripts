0697e98b131d42af46afe2a286cede3c0c4926bf
Andrej Kovac <kamee@kameecoding.com>
2020-12-24 17:33:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-sudo pacman -Syy git base-devel vlc git
+sudo pacman -Syy git base-devel vlc git --no-confirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
 rm -rf yay
-yay -Syy visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm brave dbeaver pencil staruml steam ntfs-3g logiops dropbox
+yay -Syy visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm brave dbeaver pencil staruml steam ntfs-3g logiops dropbox --no-confirm
 #filebot47 ffmpeg 
