b5c2ed0b430d548a39afece5b83b6a74d649b82e
Andrej Kovac <kamee@kameecoding.com>
2020-12-25 05:37:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cd yay
 makepkg -si
 cd ..
 rm -rf yay
-yay -Syy  --noconfirm visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm brave dbeaver pencil staruml steam ntfs-3g logiops dropbox 
+yay -Syy  --noconfirm visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm chormium dbeaver pencil staruml steam ntfs-3g logiops dropbox 
 #filebot47 ffmpeg 
