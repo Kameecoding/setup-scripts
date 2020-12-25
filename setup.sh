54fa7fadc0f272f5df646ed92e24b46c19a0623a
Andrej Kovac <kamee@kameecoding.com>
2020-12-25 06:11:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ cd ..
 rm -rf yay
 yay -Syy  --noconfirm visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm chormium dbeaver pencil staruml steam ntfs-3g logiops dropbox nordic-theme-git 
 #filebot47 ffmpeg 
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
