dfd0d63985d885fbc26424225f5b4531c98ed81e
Andrej Kovac <kamee@kameecoding.com>
2020-12-25 06:15:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ rm -rf yay
 yay -Syy  --noconfirm visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox docker nvm chormium dbeaver pencil staruml steam ntfs-3g logiops dropbox nordic-theme-git 
 #filebot47 ffmpeg 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="mortalscumbag"/g' ~/.zshrc
