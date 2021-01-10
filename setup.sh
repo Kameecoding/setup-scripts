c5e13d5d9de0ea17dd3ca534f4b751bae35ea294
Andrej Kovac <kamee@kameecoding.com>
2021-01-10 13:12:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ rm -rf yay
 yay -Syy  --noconfirm visual-studio-code-bin spotify jdk11-adoptopenjdk libreoffice jetbrains-toolbox nvm dbeaver pencil staruml logiops dropbox nordic-theme-git qbittorrent teams skypeforlinux-stable-bin ufetch insomnia
 git config --global user.email "andrej.kovac.ggc@gmail.com"
 git config --global user.name "Kameecoding"
+git config --global rebase.autoStash true
+git config --global pull.rebase true
+git config --global core.autocrlf input
 #filebot47 ffmpeg 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="mortalscumbag"/g' ~/.zshrc
@@ -20,3 +23,5 @@ echo "UUID=DA0203E20203C28D /mnt/media          ntfs-3g    defaults 0 0" | sudo 
 echo "UUID=9E5CB0D35CB0A783 /mnt/games          ntfs-3g    defaults 0 0" | sudo tee -a /etc/fstab
 sudo mount /mnt/media
 sudo mount /mnt/games
+echo 'alias gfs="git fetch && git status"' >> ~/.zshrc
+echo 'alias gpp="git pull && git push"' >> ~/.zshrc
